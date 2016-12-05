ad2304dbd3ccdebe16e3c4440fcd702bd38e7821
Enrico Weigelt, metux IT consult <enrico.weigelt@gr13.net>
2016-12-05 11:41:02 +0100

    [A] optimize simple list/map/set item removal w/o callbacks
    We have several places where we just remove list/map/set elements
    matching some criteria. The current approach of using predicate
    callback objects just adds additional temporary allocations and
    slows things down.
    
    Even worse: in some cases, elements are collected first, and then
    removed again.
    
    Just KISS - a simple, old fashioned iterator loop is enough.
    
    Rember: Java still is an imperative, not a functional language.

--- src/net/sf/freecol/common/model/DiplomaticTrade.java
+++ src/net/sf/freecol/common/model/DiplomaticTrade.java
@@ -20,6 +20,7 @@
 package net.sf.freecol.common.model;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.xml.stream.XMLStreamException;
@@ -279,7 +280,10 @@ public class DiplomaticTrade extends FreeColGameObject {
      *     {@code TradeItem} to remove.
      */
     public void removeType(Class<? extends TradeItem> itemClass) {
-        removeInPlace(this.items, matchKey(itemClass, TradeItem::getClass));
+        Iterator<TradeItem> it = this.items.iterator();
+        while (it.hasNext())
+            if (it.next().getClass() == itemClass)
+                it.remove();
     }
 
     /**
