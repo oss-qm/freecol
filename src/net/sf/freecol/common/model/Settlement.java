8a73a1eed4e589d10b5dd04f272d46f6e02ea958
Enrico Weigelt, metux IT consult <enrico.weigelt@gr13.net>
2016-12-24 20:59:19 +0100

    common: model: Settlement: optimize getHighSeasCount() w/o callbacks

--- src/net/sf/freecol/common/model/Settlement.java
+++ src/net/sf/freecol/common/model/Settlement.java
@@ -344,9 +344,19 @@ public abstract class Settlement extends GoodsLocation
      * @return A high seas count, INFINITY if not connected.
      */
     public int getHighSeasCount() {
-        Tile best = minimize(getTile().getSurroundingTiles(1, 1),
-                             Tile.isSeaTile, Tile.highSeasComparator);
-        return (best == null) ? INFINITY : best.getHighSeasCount();
+        int best_hsc = 0;
+        Tile best_tile = null;
+        for (Tile t : getTile().getSurroundingTiles(1, 1)) {
+            if (!t.isLand() && t.getHighSeasCount() >= 0) {
+                int hsc = t.getHighSeasCount();
+                if (best_tile == null || hsc < best_hsc) {
+                    best_tile = t;
+                    best_hsc = hsc;
+                }
+            }
+        }
+
+        return (best_tile == null) ? INFINITY : best_hsc;
     }
 
     /**
