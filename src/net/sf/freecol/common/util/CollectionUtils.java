3367e9399b13bb871da8bcbac360f23a0e097b2c
Enrico Weigelt, metux IT consult <enrico.weigelt@gr13.net>
2017-01-10 16:54:46 +0100

    common: util: CollectionUtils: drop obsolete toList()

--- src/net/sf/freecol/common/util/CollectionUtils.java
+++ src/net/sf/freecol/common/util/CollectionUtils.java
@@ -37,7 +37,6 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.ToDoubleFunction;
 import java.util.function.ToIntFunction;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
