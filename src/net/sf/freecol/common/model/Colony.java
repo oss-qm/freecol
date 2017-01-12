5734a25623149d5da1f0a7b129b5222dfa2e347f
Enrico Weigelt, metux IT consult <enrico.weigelt@gr13.net>
2017-01-12 05:58:37 +0100

    Java7: use Collections class for sorting

--- src/net/sf/freecol/common/model/Colony.java
+++ src/net/sf/freecol/common/model/Colony.java
@@ -2039,7 +2039,7 @@ public class Colony extends Settlement implements Nameable, TradeLocation {
         result.addAll(getBuildings());
         result.add(buildQueue);
         result.add(populationQueue);
-        result.sort(Consumer.COMPARATOR);
+        Collections.sort(result, Consumer.COMPARATOR);
         return result;
     }
 
@@ -2306,7 +2306,7 @@ loop:   for (WorkLocation wl : getWorkLocationsForProducing(goodsType)) {
                             result.add(new TileImprovementSuggestion(ct.getWorkTile(), ti, improved));
                     }
 
-        result.sort(TileImprovementSuggestion.descendingAmountComparator);
+        Collections.sort(result, TileImprovementSuggestion.descendingAmountComparator);
         return result;
     }
 
