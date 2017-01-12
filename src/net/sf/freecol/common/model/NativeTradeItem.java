e0b793eec32c62ce18b500102dd933bf4e071165
Enrico Weigelt, metux IT consult <enrico.weigelt@gr13.net>
2017-01-12 05:17:11 +0100

    common: model: NativeTradeItem: dropped obsolete goodsMatcher

--- src/net/sf/freecol/common/model/NativeTradeItem.java
+++ src/net/sf/freecol/common/model/NativeTradeItem.java
@@ -20,7 +20,6 @@
 package net.sf.freecol.common.model;
 
 import java.util.Comparator;
-import java.util.function.Predicate;
 
 import javax.xml.stream.XMLStreamException;
 
@@ -162,20 +161,6 @@ public class NativeTradeItem extends GoodsTradeItem {
     public void setHaggleCount(int haggleCount) {
         this.haggleCount = haggleCount;
     }
-
-    /**
-     * Get a predicate to match this native trade item by its goods.
-     *
-     * @return A suitable {@code Predicate}.
-     */
-    public Predicate<NativeTradeItem> goodsMatcher() {
-        return new Predicate<NativeTradeItem>() {
-            public boolean test(NativeTradeItem i) {
-                return NativeTradeItem.this.getGoods().equals(i.getGoods());
-            }
-        };
-    }
-
 
     // Interface TradeItem
     // All provided by superclass
