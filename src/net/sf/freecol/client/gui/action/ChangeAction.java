ec4df622e11ddbd6e9412d02b20027ca2d210997
Enrico Weigelt, metux IT consult <enrico.weigelt@gr13.net>
2017-01-19 00:40:13 +0100

    client: gui: ChangeAction: optimize finding list elements w/o callbacks

--- src/net/sf/freecol/client/gui/action/ChangeAction.java
+++ src/net/sf/freecol/client/gui/action/ChangeAction.java
@@ -109,11 +109,15 @@ public class ChangeAction extends UnitAction {
                     return;
                 }
             }
-            Unit active = find(tile.getUnits(),
-                               u -> (u != unit
-                                   && u.getState() == Unit.UnitState.ACTIVE
-                                   && u.getMovesLeft() > 0));
-            if (active != null) getGUI().setActiveUnit(active);
+
+            // fimxe: perhaps move that to the upper loop
+            for (Unit u : tile.getUnits()) {
+                if (u != unit && u.getState() == Unit.UnitState.ACTIVE &&
+                                 u.getMovesLeft() > 0) {
+                    getGUI().setActiveUnit(u);
+                    break;
+                }
+            }
         }
     }
 }
