85bf7836adc4e01b537c0110437173be2707a841
Enrico Weigelt, metux IT consult <enrico.weigelt@gr13.net>
2017-01-30 06:53:06 +0100

    common: model: FeatureContainer: removeFeatures() bail out on null fco

--- src/net/sf/freecol/common/model/FeatureContainer.java
+++ src/net/sf/freecol/common/model/FeatureContainer.java
@@ -415,6 +415,7 @@ public final class FeatureContainer {
      *     in.
      */
     public void removeFeatures(FreeColObject fco) {
+        if (fco == null) return;
         FeatureContainer c = fco.getFeatureContainer();
         if (c == null) return;
 
