385fdaca5c7a2c252df4beea1c964ec5405e7e83
Enrico Weigelt, metux IT consult <enrico.weigelt@gr13.net>
2017-02-13 16:41:16 +0100

    common: model: Colony: make getAvailableWorkLocationsList() threadsafe

--- src/net/sf/freecol/common/model/Colony.java
+++ src/net/sf/freecol/common/model/Colony.java
@@ -573,7 +573,18 @@ public class Colony extends Settlement implements Nameable, TradeLocation {
      * @return The list of available {@code WorkLocation}s.
      */
     public List<WorkLocation> getAvailableWorkLocationsList() {
-        return transform(getAllWorkLocations(), WorkLocation::isAvailable);
+        List<WorkLocation> result = new ArrayList<>();
+        synchronized (this.colonyTiles) {
+            for (ColonyTile walk : this.colonyTiles)
+                if (walk.isAvailable())
+                    result.add(walk);
+        }
+        synchronized (this.buildingMap) {
+            for (Building walk : this.buildingMap.values())
+                if (walk.isAvailable())
+                    result.add(walk);
+        }
+        return result;
     }
 
     /**
